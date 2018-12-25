3427afd05bcb1599fc34895a8395afee21088556
melman1996 <34185413+melman1996@users.noreply.github.com>
2018-12-25 18:21:04 +0100

    asd

--- src/main.py
+++ src/main.py
@@ -1,76 +1,79 @@
 from keras.models import Sequential
 from keras.layers import Dense
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.feature_selection import RFECV
+import pandas as pd
 import numpy as np
 import sys
+
+
+def normalize_results(primary_results):
+    results = {}
+    for county in county_facts.iloc[:, 0].values:
+        results[county] = [0] * 7
+
+    for result in primary_resaults.iloc[:, :].values:
+        if result[3] in results:
+            if result[5] == "Bernie Sanders":
+                results[result[3]][0] = result[7]
+            elif result[5] == "Hillary Clinton":
+                results[result[3]][1] = result[7]
+            elif result[5] == "Ben Carson":
+                results[result[3]][2] = result[7]
+            elif result[5] == "Donald Trump":
+                results[result[3]][3] = result[7]
+            elif result[5] == "John Kasich":
+                results[result[3]][4] = result[7]
+            elif result[5] == "Marco Rubio":
+                results[result[3]][5] = result[7]
+            elif result[5] == "Ted Cruz":
+                results[result[3]][6] = result[7]
+
+    for key in results:
+        if results[key][0] > results[key][1]:
+            results[key][0] = 1
+            results[key][1] = 0
+        elif results[key][0] < results[key][1]:
+            results[key][0] = 1
+            results[key][1] = 0
+
+        best = max(results[key][2:])
+        if best > 0:
+            for i in range(2, len(results[key])):
+                if results[key][i] == best:
+                    results[key][i] = 1
+                else:
+                    results[key][i] = 0
+    return np.array([results[key] for key in results])
 
 
 if __name__ == "__main__":
     np.random.seed(2137)
 
-    counties = np.genfromtxt("county_facts.csv", delimiter=",")
+    # read dataset from csv
+    county_facts = pd.read_csv("county_facts.csv")
+    primary_resaults = pd.read_csv("primary_results.csv")
+    X = county_facts.iloc[:, 3:].values
+    Y = normalize_results(primary_resaults)
 
-    primary = {}
-    for i in range(len(counties)):
-        primary[counties[i, 0]] = [0, 0, 0, 0, 0, 0, 0]
-        counties[i, 32] = counties[i, 32] / 62498               # change annual income to % of max
-        counties[i, 53] = counties[i, 53] / 69467.5             # change population per square mile to % of max
-        for j in range(len(counties[i])):
-            if counties[i, j] > 1:
-                counties[i, j] = counties[i, j] / 100           # change % to fractions
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
-    X = counties[:, [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 22, 23, 27, 32, 34, 53]]
+    # scale values
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
 
-    ids = np.genfromtxt("primary_results.csv", delimiter=",", usecols=[3])
-    results = np.genfromtxt("primary_results.csv", delimiter=",", usecols=[7])
-    names = np.genfromtxt("primary_results.csv", delimiter=",", dtype='str', usecols=[5])
-
-    for result, name, county in zip(results, names, ids):
-        if county in primary:
-            if name == "Bernie Sanders":
-                primary[county][0] = result
-            elif name == "Hillary Clinton":
-                primary[county][1] = result
-            elif name == "Ben Carson":
-                primary[county][2] = result
-            elif name == "Donald Trump":
-                primary[county][3] = result
-            elif name == "John Kasich":
-                primary[county][4] = result
-            elif name == "Marco Rubio":
-                primary[county][5] = result
-            elif name == "Ted Cruz":
-                primary[county][6] = result
-
-    for key in primary:
-        if primary[key][0] > primary[key][1]:
-            primary[key][0] = 1
-            primary[key][1] = 0
-        elif primary[key][0] < primary[key][1]:
-            primary[key][0] = 1
-            primary[key][1] = 0
-
-        best = max(primary[key][2:])
-        if best > 0:
-            for i in range(2, len(primary[key])):
-                if primary[key][i] == best:
-                    primary[key][i] = 1
-                else:
-                    primary[key][i] = 0
-
-    Y = np.array([
-        primary[key] for key in primary
-    ])
-
-    training_length = int(0.7 * len(X))
-
+    # create NN
     model = Sequential()
-    input = len(X[0])
-    model.add(Dense(50, input_dim=input, activation='relu'))
-    model.add(Dense(50, activation='relu'))
-    model.add(Dense(7, activation='sigmoid'))
+    model.add(Dense(output_dim=30, init='uniform', activation='relu', input_dim=51))
+    model.add(Dense(output_dim=15, init='uniform', activation='relu'))
+    model.add(Dense(output_dim=7, init='uniform', activation='sigmoid'))
 
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-    model.fit(X[:training_length], Y[:training_length], epochs=150, batch_size=20)
-    scores = model.evaluate(X[training_length:], Y[training_length:])
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    # fit the model
+    model.fit(X_train, Y_train, batch_size=10, epochs=100)
+    # evaluate NN
+    scores = model.evaluate(X_test, Y_test)
     print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
